4c3cce244c601acfe714b3f93917626d679ba686
Dzanpo <140071979+alexmoiseyev@users.noreply.github.com>
2023-11-05 16:05:58 +0300

    new site

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>GuitarsMarket</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <div class="container">
+                <div class="header__block">
+                    <ul class="header__menu">
+                        <li class="header__menu-item">
+                            <a href = "#">Главная</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href = "#">О нас</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href = "#">Каталог</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href = "#">Контакты</a>
+                        </li>
+                    </ul>
+                    <div class="header__img-block">
+                        <p class="header__img-block-title">Магазин Б/У Гитар</p>
+                        <img src="./image/header-img.jpg" alt="" class="header__img">
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="main">
+            <div class="container">
+                <h1 class="main__title">Каталог гитар</h1>
+                <div class="main__block">
+                    <div class="card">
+                        <div class="card__title">
+                            <h3>Les paul</h3>
+                        </div>
+                        <img src="./image/guitar1.jpg" alt="" class="card__img">
+                        <div class="card__price">
+                            <p class="card__price-text">$200</p>
+                        </div>
+                        <a href="#" class="card__link">Подробнее</a>
+                    </div>
+                    <div class="card">
+                        <div class="card__title">
+                            <h3>Jazz bass</h3>
+                        </div>
+                        <img src="./image/guitar2.jpg" alt="" class="card__img">
+                        <div class="card__price">
+                            <p class="card__price-text">$500</p>
+                        </div>
+                        <a href="#" class="card__link">Подробнее</a>
+                    </div>
+                    <div class="card">
+                        <div class="card__title">
+                            <h3>Clarus</h3>
+                        </div>
+                        <img src="./image/guitar3.jpg" alt="" class="card__img">
+                        <div class="card__price">
+                            <p class="card__price-text">$150</p>
+                        </div>
+                        <a href="#" class="card__link">Подробнее</a>
+                    </div>
+                    <div class="card">
+                        <div class="card__title">
+                            <h3>Acoustic</h3>
+                        </div>
+                        <img src="./image/guitar4.jpg" alt="" class="card__img">
+                        <div class="card__price">
+                            <p class="card__price-text">$50</p>
+                        </div>
+                        <a href="#" class="card__link">Подробнее</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <ul class="contacts">
+                    <li class="contact">Выполнил: Моисеев Алексей Дмитриевич</li>
+                    <li class="contact">Студент группы КСП-216</li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
