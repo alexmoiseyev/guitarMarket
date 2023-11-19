c1c223510ac0341b013afa2ea731aa20a05069a3
Dzanpo <140071979+alexmoiseyev@users.noreply.github.com>
2023-11-19 17:23:21 +0300

    newFORM

--- pages/script.js
+++ pages/script.js
@@ -3,8 +3,13 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function(){
+    if(document.contact__form.name.value && document.contact__form.phone.value != ""){
     modal.style.display = "block";
+}else{
+    alert ( "Вы не заполнили ваше имя или телефон!" );
+}
 }
 span.onclick = function(){
     modal.style.display = "none";
 }
+
